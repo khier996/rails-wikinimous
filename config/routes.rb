fb060b47b8df9db2699ba3182f31663b7b4d4f18
Maksim Khier <maksimkhier@outlook.com>
2017-03-15 09:03:02 +0800

    Finished wikiblog exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :articles
+
+  # get 'article/list'
+
+  # get 'article/show'
+
+  # get 'article/create'
+
+  # get 'article/update'
+
+  # get 'article/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
